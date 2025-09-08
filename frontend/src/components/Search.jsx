2226da302fdf49b93caa5a0e295531396037ee82
HamdiNur <hamdoonur26@gmail.com>
2025-09-08 22:05:14 +0300

    feat: added Base64 avatar display in search results

--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -1,11 +1,14 @@
-import React, { useState } from 'react';
-import { collection, getDocs, query, where } from "firebase/firestore";
-import { db } from "../firebase";
+import React, { useContext, useState } from 'react';
+import { collection, doc, getDocs, query, setDoc, where } from "firebase/firestore";
+import {  db } from "../firebase";
+import { AuthContext } from '../context/AuthContext';
+import { updateDoc, serverTimestamp, getDoc } from "firebase/firestore";
 
 const Search = () => {
   const [username, setUsername] = useState("");
   const [user, setUser] = useState(null);
   const [err, setErr] = useState(false);
+  const {currentUser}=useContext(AuthContext)
 
   const handleSearch = async () => {
     const q = query(collection(db, "users"), where("displayName", "==", username));
@@ -31,6 +34,46 @@ const Search = () => {
     if (e.code === "Enter") handleSearch();
   };
 
+  const handleSelect= async()=>{
+    //check wheether the groupChat in firestore) exists , if not create
+    const combinedId=currentUser.uid>user.uid
+    ?currentUser.uid+ user.uid
+   : user.uid +currentUser.uid;
+   try {
+const res = await getDoc(doc(db,"chats",combinedId));
+if (!res.exists()) {
+  //create chats in chats collection
+await setDoc(doc(db, "chats", combinedId), { messages: [] });
+   // create user chats
+await updateDoc(doc(db,"userChats",currentUser.uid),{
+  [combinedId+".userInfo"]:{
+    uid:user.uid,
+    displayName:user.displayName,
+    photoUrl:user.avater
+  },
+  [combinedId+".date"]:serverTimestamp()
+});
+await updateDoc(doc(db,"userChats",user.uid),{
+  [combinedId+".userInfo"]:{
+    uid:currentUser.uid,
+    displayName:currentUser.displayName,
+    photoUrl:currentUser.avater
+  },
+  [combinedId+".date"]:serverTimestamp()
+});
+
+
+
+}
+   } catch (error) {
+    
+   }
+    // create user chats
+      setUser(null);
+  setUsername("")
+  }
+
+
   return (
     <div className='search'>
       <div className="searchForm">
@@ -39,13 +82,14 @@ const Search = () => {
           placeholder='Find a user'
           onKeyDown={handleKey}
           onChange={(e) => setUsername(e.target.value)}
+          value={username}
         />
       </div>
 
       {err && <span>User Not Found</span>}
 
       {user && (
-        <div className="userChat">
+        <div className="userChat" onClick={handleSelect}>
           <img
             src={user.avatar && user.avatar.trim() !== "" ? user.avatar : "/defaultAvatar.png"}
             alt="User Avatar"
