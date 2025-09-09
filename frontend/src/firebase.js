5e47b0594b7d996ea16c8c741544085ca3ab8ca6
HamdiNur <hamdoonur26@gmail.com>
2025-09-09 21:47:58 +0300

    feat: setup Firebase config using environment variables

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -4,15 +4,15 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAeFsUuxpgyltojOFd30iWs4jUpG9hEdZA",
-  authDomain: "chat-3501e.firebaseapp.com",
-  projectId: "chat-3501e",
-  storageBucket: "chat-3501e.firebasestorage.app",
-  messagingSenderId: "625233650879",
-  appId: "1:625233650879:web:528225f2751f5b12e6920a"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth();
 export const storage = getStorage();
-export const db=getFirestore();
+export const db = getFirestore();
