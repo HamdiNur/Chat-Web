4f1197b7174be44e1f68f0192697ee581fb6fd76
HamdiNur <hamdoonur26@gmail.com>
2025-09-10 11:26:12 +0300

    feat: implement Messages component to render chat messages list

--- frontend/src/components/Messages.jsx
+++ frontend/src/components/Messages.jsx
@@ -1,25 +1,36 @@
-import React from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import Message from './Message';
+import { doc, onSnapshot } from 'firebase/firestore';
+import { ChatContext } from '../context/ChatContext';
+import { db } from '../firebase';
 
 const Messages = () => {
+  const [messages, setMessages] = useState([]);
+  const { data } = useContext(ChatContext);
+
+  useEffect(() => {
+    if (!data.chatId || data.chatId === "null") return;
+
+    const unSub = onSnapshot(doc(db, "chats", data.chatId), (doc) => {
+      if (doc.exists()) {
+        setMessages(doc.data().messages);
+      } else {
+        setMessages([]);
+      }
+    });
+
+    return () => unSub();
+  }, [data.chatId]);
+
+  console.log(messages);
+
   return (
     <div className='messages'>
-        <Message/>
-         <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-
-      
+      {messages.map((m, index) => (
+        <Message message={m} key={index} />
+      ))}
     </div>
   );
-}
+};
 
 export default Messages;
